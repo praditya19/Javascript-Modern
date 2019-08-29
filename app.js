192d66d4e31a5e222d87b4502f4b03dfcefbad11
praditya <praditya.rendi88@gmail.com>
2019-08-29 23:02:33 +0700

    tipe data string

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-//variable + angka
-let originNumber = 23;
-let newNumber = originNumber + 7;
-
-console.log(newNumber);
-
-//variable + variable
-let data = 30;
-let datas = 60;
-let newData = data + datas;
-
-console.log(newData);
+let visitor = prompt('Siapakah namamu?');
+let message =
+  'Halo ' +
+  visitor +
+  ', Selamat datang di Javascript Modern ' +
+  'Saya senang sekali kamu datang berkunjung' +
+  ', Semoga kamu berkenan datang kembali.';
+alert(message);
